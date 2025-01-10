98fe481f9cf3a26105d586f0df955d09a62badfe
Tmazst <55834770+Tmazst@users.noreply.github.com>
2025-01-10 19:03:51 +0200

    whatsapp link fixing

--- app.py
+++ app.py
@@ -2599,39 +2599,43 @@ def church_announcements():
 userr = admin_user
 def generate_whatsapp_link(announce, user, church):
     text = None
-    
-    if userr.role == 'admin_user':
-        if userr.query.get(announce.usr_id).name:
-            text = (
+    encoded_text = None
+    try:
+        if userr.role == 'admin_user':
+            if userr.query.get(announce.usr_id).name:
+                text = (
+                    f"\n*CHURCH ANNOUNCEMENT* \n"
+                    f"\n*{announce.title}* \n\n"
+                    f"{announce.info}\n\n"
+                    f"_By: {userr.query.get(announce.usr_id).name} - _"
+                    f"_{userr.query.get(announce.edited_by).committee_local_group}_ "
+                    f"_{userr.query.get(announce.edited_by).committee_local_pos}_\n\n"
+                    f"*{church.church_name}*\n"
+                    f"*Contancts:* {church.church_contacts}\n"
+                    f"*Email:* {church.church_email}\n\n\n"
+                    f"Shared from: CAMM Sys+\n"
+                    f"https://camm.churchregistry.org/announcements"
+                )
+            else:
+                
+                text = (
                 f"\n*CHURCH ANNOUNCEMENT* \n"
-                f"\n*{announce.title}* \n\n"
+                f"*\n{announce.title}* \n\n"
                 f"{announce.info}\n\n"
-                f"_By: {userr.query.get(announce.usr_id).name} - _"
-                f"_{userr.query.get(announce.edited_by).committee_local_group}_ "
-                f"_{userr.query.get(announce.edited_by).committee_local_pos}_\n\n"
                 f"*{church.church_name}*\n"
                 f"*Contancts:* {church.church_contacts}\n"
                 f"*Email:* {church.church_email}\n\n\n"
                 f"Shared from: CAMM Sys+\n"
-                f"https://camm.churchregistry.org/announcements"
-            )
-        else:
-            f"\n*CHURCH ANNOUNCEMENT* \n"
-            text = (
-            f"*\n{announce.title}* \n\n"
-            f"{announce.info}\n\n"
-            f"*{church.church_name}*\n"
-            f"*Contancts:* {church.church_contacts}\n"
-            f"*Email:* {church.church_email}\n\n\n"
-            f"Shared from: CAMM Sys+\n"
-            f"https://camm.churchregistry.org/church_announcements"
-            )
-        
-        encoded_text = text.encode('utf-8')
+                f"https://camm.churchregistry.org/church_announcements"
+                )
+            
+            encoded_text = text.encode('utf-8')
 
-        encoded_text = quote(encoded_text)
+            encoded_text = quote(encoded_text)
 
-    return f"https://wa.me/?text={encoded_text}"
+        return f"https://wa.me/?text={encoded_text}"
+    except:
+        return "None"
 
 
 @app.route("/announcements_form", methods=["POST","GET"])
